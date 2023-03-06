3016790eab0d834cb4a6635003865e42a6939101
= <Avinash.Kumar@baptisthealth.net>
2023-03-06 22:33:52 +0530

    Completed the app integrated all the test cases of api and widget

---
+++ test/omdb_movie_detail_api_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nock/nock.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  setUpAll(() {
+    nock.init();
+  });
+
+  setUp(() {
+    nock.cleanAll();
+  });
+
+  test('OMDB Detail API Test', () async {
+    final interceptor = nock('http://www.omdbapi.com/?apikey=50770fd4&type=Movie').get('&i=tt0118688')
+      ..reply(
+        200,
+        'Title',
+      );
+
+    final response = await http.get(Uri.parse('http://www.omdbapi.com/?apikey=50770fd4&type=Movie&i=tt0118688'));
+    expect(interceptor.isDone, true);
+    expect(response.statusCode, 200);
+    expect(response.body, 'Title');
+  });
+}
--- test/omdb_text_widget_test.dart
+++ test/omdb_text_widget_test.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:movie_omdb/main.dart';
 import 'package:movie_omdb/widgets/omdb_txt_widget.dart';
 
 const Key omdbTextWidgetKey = Key('omdb-text-widget');
 
 void main() {
   testWidgets('finds a Text on OMDB App Widget', (WidgetTester tester) async {
-    // Build an App with a Text Widget that displays the letter 'H'
+    // Build an App with a Text Widget that displays the letter 'OMDB Movies'
     await tester.pumpWidget(const MaterialApp(
       home: Scaffold(
         body: OMDBTxtWidget(
             key: omdbTextWidgetKey,
-            title: 'H',
+            title: 'OMDB Movies',
             fontSize: 16,
             txtColor: Colors.red
         ),
       ),
     ));
     // Find a Widget that displays the letter 'H'
-    expect(find.text('H'), findsOneWidget);
+    expect(find.text('OMDB Movies'), findsOneWidget);
   });
 }
